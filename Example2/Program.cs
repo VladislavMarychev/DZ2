f98d2937e271fa1c1e156fc07c4cb67f82e8a325
VladislavMarychev <vladislav.marychev@yandex.ru>
2023-07-09 20:44:18 +0300

    Второе задание готов

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,19 +1,21 @@
 ﻿// Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите трехначное число");
+Console.WriteLine("Введите число");
 int numberA = Convert.ToInt32(Console.ReadLine());
 int third = 1;
-    if (numberA > 999)
+    while (numberA > 999)
     {
     numberA /= 10;
+    
     }
     if (numberA < 100)
     {
-        Console.WriteLine("Маленькое число");
+        Console.WriteLine("Вы ввели слишком маленькое число");
+        return;
     }
     else
 {
 third = numberA % 10;
 }
 
-Console.WriteLine("Цифра {0}" , third);
+Console.WriteLine("Третья цифра этого числа {0}" , third);
